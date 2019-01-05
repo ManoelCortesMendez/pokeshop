3fe28c19b5330f7f65b8c9e5fda0d8e50a57e827
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2019-01-05 18:48:22 +0100

    Set listener to open editor on FAB click

--- app/src/main/java/com/example/android/pokeshop/MainActivity.java
+++ app/src/main/java/com/example/android/pokeshop/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.pokeshop;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Inflate activity contents
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Open editor on FAB click
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View clickedView) {
+                Intent openEditorIntent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(openEditorIntent);
+            }
+        });
     }
 }

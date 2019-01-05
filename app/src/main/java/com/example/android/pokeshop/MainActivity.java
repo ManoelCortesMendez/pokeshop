c3ac5e70f3d67529fe3c1db3e27e647dd5b0a1db
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2019-01-05 18:55:43 +0100

    Add options menu to main activity

--- app/src/main/java/com/example/android/pokeshop/MainActivity.java
+++ app/src/main/java/com/example/android/pokeshop/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,18 +12,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        // Inflate activity contents
+        // Inflate activity content
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Open editor on FAB click
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Open editor on FAB click.
+             *
+             * @param clickedView View clicked.
+             */
             @Override
             public void onClick(View clickedView) {
                 Intent openEditorIntent = new Intent(MainActivity.this, EditorActivity.class);
                 startActivity(openEditorIntent);
             }
         });
+    }
+
+    /**
+     * Inflate options menu.
+     *
+     * @param menu Activity's menu.
+     * @return true to add menu to app bar.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true; // To add menu to app bar
     }
 }
